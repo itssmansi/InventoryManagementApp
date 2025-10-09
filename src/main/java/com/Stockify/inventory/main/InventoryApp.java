2be4be276c5b70918dc25386960551891d194827
itssmansi <cmansi2310@gmail.com>
2025-10-09 23:17:53 +0530

    Adding a demo GUI for reference

--- src/main/java/com/Stockify/inventory/main/InventoryApp.java
+++ src/main/java/com/Stockify/inventory/main/InventoryApp.java
@@ -1,7 +1,10 @@
 package com.Stockify.inventory.main;
 
+import com.Stockify.inventory.gui.DemoForm;
+
+
 public class InventoryApp {
     public static void main(String[] args) {
-        System.out.println("Inventory Management App started!");
+        new DemoForm();
     }
 }
