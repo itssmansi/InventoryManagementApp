f7ec5e985c38dd03cc62231258dacbc473dd6507
itssmansi <cmansi2310@gmail.com>
2025-10-09 22:27:45 +0530

    Add InventoryApp.java in main folder

---
+++ src/main/java/com/Stockify/inventory/main/InventoryApp.java
@@ -0,0 +1,7 @@
+package com.Stockify.inventory.main;
+
+public class InventoryApp {
+    public static void main(String[] args) {
+        System.out.println("Inventory Management App started!");
+    }
+}
